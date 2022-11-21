fafc3f0de443e18418f0ce61fd93fa84e3b42a58
yj776 <yj776@nyu.edu>
2022-11-21 17:59:33 -0500

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -26,34 +26,8 @@ router.get('/', (req,res) => {
 module.exports = router
 
 
-// GET /places
-// router.get('/',(req,res) => {
-//     // res.send('GET/places')
-//     res.render('places/index')
-// })
-// GET /places
-// router.get('/', (req, res) => {
-//     res.render('places/index')
-//   })
-
-  // GET /places
-// app.get('/', (req,res) => {
-//     let places = [{
-//         name: 'H-Thai-ML',
-//         city: 'Seattle',
-//         state: 'WA',
-//         cuisines: 'Thai, Pan-Asian',
-//         pic: 'http://placekitten.com/250/250'
-//       }, {
-//         name: 'Coding Cat Cafe',
-//         city: 'Phoenix',
-//         state: 'AZ',
-//         cuisines: 'Coffee, Bakery',
-//         pic: 'http://placekitten.com/250/250'
-//       }]      
-//       res.render('places/index', {places})
-// })
-
-
-
+// Get /places/new route
+router.get('/new', (req,res) => {
+  res.render('places/new')
+})
 
