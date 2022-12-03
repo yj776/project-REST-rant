b12388791d8d8979664543dcb8b16f10b9b594a7
yj776 <yj776@nyu.edu>
2022-12-03 13:12:04 -0500

    Show Route connected

--- controllers/places.js
+++ controllers/places.js
@@ -27,7 +27,12 @@ module.exports = router
 
 
 // Get /places/new route
-router.get('/new', (req,res) => {
-  res.render('places/new')
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show')     
+  }
 })
-
