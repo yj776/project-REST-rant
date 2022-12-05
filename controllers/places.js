3abd882ca158598d1c54a034171755922701e9d1
Amii Jiang <amii@MacBook-Pro.fios-router.home>
2022-12-05 13:42:28 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,25 +1,25 @@
 // const express = require('express')
 // const app = express()
 const router = require('express').Router()
-// const places = require('../models')
+const places = require('../models/places.js')
 
 // Get /places
 router.get('/', (req,res) => {
-      let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai-food.jpg'
-        // pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cat-cafe.jpg'
-        // pic: 'http://placekitten.com/250/250'
-      }]
+      // let places = [{
+      //   name: 'H-Thai-ML',
+      //   city: 'Seattle',
+      //   state: 'WA',
+      //   cuisines: 'Thai, Pan-Asian',
+      //   pic: '/images/thai-food.jpg'
+      //   // pic: 'http://placekitten.com/250/250'
+      // }, {
+      //   name: 'Coding Cat Cafe',
+      //   city: 'Phoenix',
+      //   state: 'AZ',
+      //   cuisines: 'Coffee, Bakery',
+      //   pic: '/images/cat-cafe.jpg'
+      //   // pic: 'http://placekitten.com/250/250'
+      // }]
   
     res.render('places/index', {places})
 })
@@ -32,8 +32,22 @@ router.get('/new', (req, res) => {
 // Post/ place route
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
+
+
 
 
 // Get /places/show route
@@ -42,10 +56,14 @@ router.get('/:id', (req, res) => {
   if (isNaN(id)) {
     res.render('error404')
   }
+  else if (!places[id]) {
+    res.render('error404')
+  }
   else {
-    res.render('places/show', {place:places[id],id})     
+    res.render('places/show', {place: places[id]})
   }
 })
+
 
 
 module.exports = router
