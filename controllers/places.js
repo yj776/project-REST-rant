dd370804ff7184f8acd95799be47b7681b59a57d
yj776 <yj776@nyu.edu>
2022-12-07 19:22:50 -0500

    implemented mongoose ODM

--- controllers/places.js
+++ controllers/places.js
@@ -1,121 +1,192 @@
-// const express = require('express')
-// const app = express()
 const router = require('express').Router()
-const places = require('../models/places.js')
+const db = require('../models')
 
-// Get /places
-router.get('/', (req,res) => {
-      // let places = [{
-      //   name: 'H-Thai-ML',
-      //   city: 'Seattle',
-      //   state: 'WA',
-      //   cuisines: 'Thai, Pan-Asian',
-      //   pic: '/images/thai-food.jpg'
-      //   // pic: 'http://placekitten.com/250/250'
-      // }, {
-      //   name: 'Coding Cat Cafe',
-      //   city: 'Phoenix',
-      //   state: 'AZ',
-      //   cuisines: 'Coffee, Bakery',
-      //   pic: '/images/cat-cafe.jpg'
-      //   // pic: 'http://placekitten.com/250/250'
-      // }]
-  
-    res.render('places/index', {places})
+router.get('/', (req, res) => {
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
 })
 
-// places/new route
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-// Post/ place route
-router.post('/', (req, res) => {
-  console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-// Get /places/show route
 router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
-})
-
-// Delete Route
-router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
-})
-
-// Edit Route
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
       res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id],id })
-  }
+  })
 })
 
-// Put Route
+
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
-
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
+  res.send('PUT /places/:id stub')
 })
 
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
 
 module.exports = router
+
+
+
+
+
+
+// -----------------------------------------
+
+// // const express = require('express')
+// // const app = express()
+// const router = require('express').Router()
+// const places = require('../models/places.js')
+
+// // Get /places
+// router.get('/', (req,res) => {
+//       // let places = [{
+//       //   name: 'H-Thai-ML',
+//       //   city: 'Seattle',
+//       //   state: 'WA',
+//       //   cuisines: 'Thai, Pan-Asian',
+//       //   pic: '/images/thai-food.jpg'
+//       //   // pic: 'http://placekitten.com/250/250'
+//       // }, {
+//       //   name: 'Coding Cat Cafe',
+//       //   city: 'Phoenix',
+//       //   state: 'AZ',
+//       //   cuisines: 'Coffee, Bakery',
+//       //   pic: '/images/cat-cafe.jpg'
+//       //   // pic: 'http://placekitten.com/250/250'
+//       // }]
+  
+//     res.render('places/index', {places})
+// })
+
+// // places/new route
+// router.get('/new', (req, res) => {
+//   res.render('places/new')
+// })
+
+// // Post/ place route
+// router.post('/', (req, res) => {
+//   console.log(req.body)
+//   if (!req.body.pic) {
+//     // Default image if one is not provided
+//     req.body.pic = 'http://placekitten.com/400/400'
+//   }
+//   if (!req.body.city) {
+//     req.body.city = 'Anytown'
+//   }
+//   if (!req.body.state) {
+//     req.body.state = 'USA'
+//   }
+//   places.push(req.body)
+//   res.redirect('/places')
+// })
+
+// // Get /places/show route
+// router.get('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     res.render('places/show', { place: places[id], id })
+//   }
+// })
+
+// // Delete Route
+// router.delete('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     places.splice(id, 1)
+//     res.redirect('/places')
+//   }
+// })
+
+// // Edit Route
+// router.get('/:id/edit', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//       res.render('error404')
+//   }
+//   else if (!places[id]) {
+//       res.render('error404')
+//   }
+//   else {
+//     res.render('places/edit', { place: places[id],id })
+//   }
+// })
+
+// // Put Route
+// router.put('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//       res.render('error404')
+//   }
+//   else if (!places[id]) {
+//       res.render('error404')
+//   }
+//   else {
+//       // Dig into req.body and make sure data is valid
+//       if (!req.body.pic) {
+//           // Default image if one is not provided
+//           req.body.pic = 'http://placekitten.com/400/400'
+//       }
+//       if (!req.body.city) {
+//           req.body.city = 'Anytown'
+//       }
+//       if (!req.body.state) {
+//           req.body.state = 'USA'
+//       }
+
+//       // Save the new data into places[id]
+//       places[id] = req.body
+//       res.redirect(`/places/${id}`)
+//   }
+// })
+
+
+// module.exports = router
--- models/places.js
+++ models/places.js
@@ -1,15 +1,28 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai-food.jpg'
-    // pic: 'http://placekitten.com/250/250'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cat-cafe.jpg'
-    // pic: 'http://placekitten.com/250/250'
-  }]
+// module.exports = [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '/images/thai-food.jpg'
+//     // pic: 'http://placekitten.com/250/250'
+//   }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/cat-cafe.jpg'
+//     // pic: 'http://placekitten.com/250/250'
+//   }]
+
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
